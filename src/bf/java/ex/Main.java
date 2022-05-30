5022338ebf6664304ac047a10231673564694b53
rvdemael <romain.v@live.be>
2022-05-30 18:46:34 +0200

    cleaning files

--- src/bf/java/ex/Main.java
+++ src/bf/java/ex/Main.java
@@ -1,21 +1,8 @@
 package bf.java.ex;
 
-import bf.java.ex.delegate.Action;
-import bf.java.ex.shop.Shop;
-
 public class Main {
     public static void main(String[] args) {
         GameMaster gm = new GameMaster();
-
-
-        //Spell s = new Spell(2,5);
-
-        //demo(Map::Deplacer)
-        //demo(pos -> map.deplacer);
-        //demo(System.out::println);
     }
 
-    public static void demo(Action<Integer> action) {
-        action.execute(42);
-    }
 }
--- src/bf/java/ex/delegate/Action.java
+++
@@ -1,6 +0,0 @@
-package bf.java.ex.delegate;
-
-@FunctionalInterface
-public interface Action<T> {
-    void execute(T obj);
-}
